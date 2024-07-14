d10a42d127a3c4d3c205babd8952fb85686d3f65
Sergey <serg_zv@msn.com>
2024-07-14 16:16:32 +0300

    загрузка

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name_1", "Last_Name_1", (byte) 20);
+        userDao.saveUser("Name_2", "Last_Name_2", (byte) 25);
+        userDao.saveUser("Name_3", "Last_Name_3", (byte) 31);
+        userDao.saveUser("Name_4", "Last_Name_4", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
